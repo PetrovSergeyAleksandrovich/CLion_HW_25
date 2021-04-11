399026cbb062db3d1db04da194166a010fa012c7
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-04-11 13:38:36 +0300

    task_1 done

--- HW25_task_1.cpp
+++ HW25_task_1.cpp
@@ -1,2 +1,26 @@
+#include <iostream>
+#include <map>
 
+int main()
+{
 
+    std::map<std::string, std::string> dict;
+
+    dict.insert(std::make_pair<std::string, std::string>("12-34-56", "Ivanov"));
+    dict.insert(std::make_pair<std::string, std::string>("98-76-54", "Petrov"));
+    dict.insert(std::make_pair<std::string, std::string>("13-57-91", "Sidorov"));
+    dict.insert(std::make_pair<std::string, std::string>("15-86-67", "Ivanov"));
+    dict.insert(std::make_pair<std::string, std::string>("24-68-10", "Laptev"));
+
+    std::cout << "Type surname or phone number:";
+    std::string user_input;
+    std::cin >> user_input;
+
+    for( auto ITR = dict.begin(); ITR != dict.end(); ITR++)
+    {
+        if(user_input == ITR->second) std::cout << ITR->first << std::endl;
+        if(user_input == ITR->first) std::cout << ITR->second << std::endl;
+    }
+
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <map>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    std::map<int, std::map<int, char> > dict;
+
+    for(int i = 0; i < 127; i++)
+        for(int j = 0; j < 127; j++)
+            dict[i][j] = (char)j;
+
+    std::cout << dict[99][107];
+
     return 0;
 }
+
