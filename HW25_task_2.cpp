e09d1c114968d17226c82d83ca5f08931fb82f01
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-04-11 15:08:53 +0300

    task_2 done

---
+++ HW25_task_2.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <map>
+
+int main()
+{
+    std::string user_input;
+    int counter_to_show = 1;
+    int counter_to_add = 0;
+    std::map<std::string, int> dict;
+
+    //START Init dictionary and sort them
+    dict.insert(std::make_pair<std::string, int>("Sidorov", 1));
+    dict.insert(std::make_pair<std::string, int>("Petrov", 2));
+    dict.insert(std::make_pair<std::string, int>("Agafonov", 3));
+    dict.insert(std::make_pair<std::string, int>("Laptev", 4));
+    for(std::map<std::string, int>::iterator ITR = dict.begin(); ITR != dict.end(); ITR++)
+    {
+        ITR->second = ++counter_to_add;
+    }
+    counter_to_add = 0;
+    //END Init dictionary and sort them
+
+    while(user_input != "exit")
+    {
+
+        std::cout << "Current list:" << std::endl;
+        for(std::map<std::string, int>::iterator ITR = dict.begin(); ITR != dict.end(); ITR++)
+        {
+            std::cout << "<- " << ITR->first << " " << ITR->second << std::endl;
+        }
+
+        std::cout << "\nInput Surname or type Next to get next person in the list: ";
+        std::cin >> user_input;
+
+        if(user_input == "Next" || user_input == "next")
+        {
+            //Show next or show from new added person
+            for(std::map<std::string, int>::iterator ITR = dict.begin(); ITR != dict.end(); ITR++)
+            {
+                if(counter_to_show == ITR->second)
+                {
+                    std::cout << "<- " << ITR->first << std::endl << std::endl;
+                    counter_to_show++;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            //Add new person
+            dict.insert(std::pair<std::string, int>(user_input, 0));
+            for(std::map<std::string, int>::iterator ITR = dict.begin(); ITR != dict.end(); ITR++)
+            {
+                ITR->second = ++counter_to_add;
+            }
+            for(std::map<std::string, int>::iterator ITR = dict.begin(); ITR != dict.end(); ITR++)
+            {
+                if(ITR->first == user_input) counter_to_show = ITR->second;
+            }
+            counter_to_add = 0;
+        }
+
+
+    }
+
+}
+
